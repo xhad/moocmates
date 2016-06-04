39062d59852e12f3fa6ab93e5da32fc9e6ae6d00
xhad <chyad@hotmail.com>
2016-06-04 10:32:45 +0900

    stuff

--- www/js/services.js
+++ www/js/services.js
@@ -14,27 +14,27 @@ angular.module('moocmates.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: 'xhad',
     lastText: 'You on your way?',
     face: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
+    name: 'Nate',
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: 'Adam',
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: 'Marissa',
     lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
+    face: 'img/girl.jpg'
   }, {
     id: 4,
-    name: 'Mike Harrington',
+    name: 'Hong Man',
     lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png'
   }];
